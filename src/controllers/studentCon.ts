26f68d663298760422833f9310cfa1fedfb59c90
Craig <indigomx9@gmail.com>
2021-07-31 23:28:35 -0400

    Added the CRUD Routes and Controllers.

--- src/controllers/studentCon.ts
+++ src/controllers/studentCon.ts
@@ -28,6 +28,48 @@ async (req, res, next) => {
     }
 };
 
+export const GetOneStudent: RequestHandler =
+async (req, res, next) => {
+    try {
+        const student: Student = 
+            await Student.findOneOrFail(req.params.id);
+        return res.status(201).json(student);
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+        next(error);
+    }
+};
+
+export const UpdateStudent: RequestHandler =
+async (req, res, next) => {
+    try {
+        const student: Student =
+            await Student.findOneOrFail(req.params.id);
+        student.firstName = req.body.firstName;
+        student.lastName = req.body.lastName;
+        student.age = req.body.age;
+        student.info = req.body.info;
+        await student.save();
+        return res.status(200).json(student);
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+        next(error);
+    }
+};
+
+export const DeleteStudent: RequestHandler =
+async (req, res, next) => {
+    try {
+        const student: Student =
+            await Student.findOneOrFail(req.params.id);
+        await student.remove();
+        return res.status(200)
+        .json("The Student was Deleted!");
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+        next(error);
+    }
+};
 
 
 
--- src/routes/studentRt.ts
+++ src/routes/studentRt.ts
@@ -1,10 +1,13 @@
 import express from "express";
-import { CreateStudent, 
-    GetAllStudents } from "../controllers/studentCon";
+import { CreateStudent, DeleteStudent, GetAllStudents, 
+    GetOneStudent, UpdateStudent } from "../controllers/studentCon";
 
 export const studentRt: express.Router = express.Router();
     studentRt.post("/", CreateStudent);
     studentRt.get("/", GetAllStudents);
+    studentRt.get("/:id", GetOneStudent);
+    studentRt.put("/:id", UpdateStudent);
+    studentRt.delete("/:id", DeleteStudent);
 
 
 
